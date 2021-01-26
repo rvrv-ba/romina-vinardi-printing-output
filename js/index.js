57e5601e712a9c55e1bec60ee35baece52cfde7b
rvrv-ba <77509313+rvrv-ba@users.noreply.github.com>
2021-01-26 15:59:19 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,57 @@
+let a = 3;
+let b = 5;
+let c;
+
+
+let output = '';
+output += 'let a = 3;';
+output += '\n';
+output += 'let b = 5;';
+output += '\n';
+output += 'let c;';
+output += '\n';
+output += '-------------------';
+output += '\n';
+output += 'a + b = ' + (a + b);
+output += '\n';
+output += 'a - b = ' + (a - b);
+output += '\n';
+output += 'a * b = ' + (a * b);
+output += '\n';
+output += 'a / b = ' + (a / b);
+output += '\n';
+output += 'a % b = ' + (a % b);
+output += '\n';
+output += 'a += b = ' + (a += b);
+output += '\n';
+output += 'a -= b = ' + (a -= b);
+output += '\n';
+output += 'a *= b = ' + (a *= b);
+output += '\n';
+output += 'a /= b = ' + (a /= b);
+output += '\n';
+output += 'a %= b = ' + (a %= b);
+output += '\n';
+output += 'a == b = ' + (a == b);
+output += '\n';
+output += 'a != b = ' + (a != b);
+output += '\n';
+output += '(a > b): ' + (a > b);
+output += '\n';
+output += '(a < b): ' + (a < b);
+output += '\n';
+output += '(!a && !c): ' + (!a && !c);
+output += '\n';
+output += '(!a || !c): ' + (!a || !c);
+output += '\n';
+
+alert(output);
+
+let output2 = '';
+let first_name = 'Romina';
+let last_name = 'Vinardi';
+let email = 'vina0004@algonquinlive.com';
+output2 += 'My name is ' + first_name + ' ' + last_name + ". " + 'You can contact me at ' + email;
+
+alert(output2);
+
